2cbc1973029d6052d699a24c1d608105a405422c
Robert Greiner <robert@robertgreiner.com>
2010-09-03 16:47:49 -0500

    Added the algorithms to calculate the total monthly payment for a home loan.

--- LoanGeek/Models/LoanData.cs
+++ LoanGeek/Models/LoanData.cs
@@ -9,13 +9,61 @@ namespace LoanGeek.Models {
         public double Principal { get; set; }
         public double InterestRate { get; set; }
         public int LoanTerm { get; set; }
-        public float PropertyTaxPercent { get; set; }
-        public float PmiPercent { get; set; }
-        public float HoaDuesYearly { get; set; }
-        public float InsuranceYearly { get; set; }
+        public double PropertyTaxPercent { get; set; }
+        public double PmiPercent { get; set; }
+        public double HoaDuesYearly { get; set; }
+        public double InsuranceYearly { get; set; }
 
-        double MonthlyInterestMultiplier { get; set; }
-        int AmortizedMonths { get; set; }
+        public double MonthlyInterestMultiplier { get; private set; }
+        public int AmortizedMonths { get; private set; }
+        public double MonthlyPayment { get; private set; }
 
+        /**
+         * How to Calculate Loan Amortization Schedules/Tables by Hand
+         * http://www.hughchou.org/calc/formula.html
+         * 
+         * P = Principal
+         * I = Annual interest rate
+         * L = Loan Term (years)
+         * 
+         * J = Monthly interest in decimal form. 
+         * J = I / (12 * 100)
+         * 
+         * N = Number of months over which the loan is amortized.
+         * N = L * 12
+         * 
+         * M = Monthly Payment
+         * 
+         * M = P * ( J / ( 1 - (1 + J)^-N ) )
+         */
+        private double CalculatePrincipalAndInterest() {
+            double principleAndInterest = 0.0;
+
+            MonthlyInterestMultiplier = InterestRate / (12 * 100);
+            AmortizedMonths = LoanTerm * 12;
+
+            double amortizedExpression = (1 - (Math.Pow((1 + MonthlyInterestMultiplier), -AmortizedMonths)));
+            principleAndInterest = Principal * (MonthlyInterestMultiplier / amortizedExpression);
+
+            return principleAndInterest;
+        }
+
+        private double CalculatePropertyTax() {
+            double propertyTax = (Principal * (InterestRate / 100)) / 12;
+            return propertyTax;
+        }
+
+        private double CalculatePmi() {
+            double pmi = (Principal * (PmiPercent / 100)) / 12;
+            return pmi;
+        }
+
+        private void CalculateTotalMonthlyPayment() {
+            MonthlyPayment = CalculatePrincipalAndInterest();
+            MonthlyPayment += CalculatePropertyTax();
+            MonthlyPayment += CalculatePmi();
+            MonthlyPayment += HoaDuesYearly / 12;
+            MonthlyPayment += InsuranceYearly / 12;
+        }
     }
 }
