76a20b99b62f5f94b52ef86ff52a27f7128d1b04
Robert Greiner <robert@robertgreiner.com>
2010-09-03 16:31:15 -0500

    Added LoanData in the model.  This is where the actual monthly payment calculations will take place.

---
+++ LoanGeek/Models/LoanData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LoanGeek.Models {
+    public class LoanData {
+
+        public double Principal { get; set; }
+        public double InterestRate { get; set; }
+        public int LoanTerm { get; set; }
+        public float PropertyTaxPercent { get; set; }
+        public float PmiPercent { get; set; }
+        public float HoaDuesYearly { get; set; }
+        public float InsuranceYearly { get; set; }
+
+        double MonthlyInterestMultiplier { get; set; }
+        int AmortizedMonths { get; set; }
+
+    }
+}
