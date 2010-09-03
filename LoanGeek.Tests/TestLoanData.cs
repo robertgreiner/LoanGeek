704e432e23ba2494e224a69b384d5f6b62521134
Robert Greiner <robert@robertgreiner.com>
2010-09-03 17:27:52 -0500

    Added unit testing project for nUnit tests.

---
+++ LoanGeek.Tests/TestLoanData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace UnitTests {
+
+    [TestFixture]
+    public class TestLoanData {
+
+        [Test]
+        public void TestMonthlyPayment() {
+
+        }
+
+        [Test]
+        public void TestMonthlyInterestPayment() {
+
+        }
+
+        [Test]
+        public void TestMonthlyPrincipalPayyment() {
+
+        }
+    }
+}
